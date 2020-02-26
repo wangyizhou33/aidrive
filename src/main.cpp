bcf9e7dfb195a4185b97817e79b00fab34eca076
Yizhou Wang <yizhouw@nvidia.com>
2020-02-25 19:13:14 -0800

    confirm eigen can be included

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <Eigen/Dense>
 
 static void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
@@ -15,6 +18,15 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action,
 }
 
 int main(void) {
+
+    using Eigen::MatrixXd;
+    MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 2.5;
+    m(0, 1) = -1;
+    m(1, 1) = m(1, 0) + m(0, 1);
+    std::cout << m << std::endl;
+
     GLFWwindow* window;
 
     glfwSetErrorCallback(error_callback);
