1a6864e8df417bf5f86121f2622733c0e0e842fe
Yizhou Wang <yizhouw@nvidia.com>
2020-04-13 21:31:03 -0700

    add imgui in app

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <GL/glew.h> // Initialize with glewInit()
 
 #include <iostream>
 #include <stdio.h>
@@ -7,18 +8,24 @@
 
 #include <Eigen/Dense>
 
-static void error_callback(int error, const char* description) {
+#include <imgui.h>
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
+
+static void error_callback(int error, const char* description)
+{
     fprintf(stderr, "Error: %s\n", description);
 }
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action,
-                         int mods) {
+                         int mods)
+{
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
-int main(void) {
-
+void testEigen()
+{
     using Eigen::MatrixXd;
     MatrixXd m(2, 2);
     m(0, 0) = 3;
@@ -26,6 +33,12 @@ int main(void) {
     m(0, 1) = -1;
     m(1, 1) = m(1, 0) + m(0, 1);
     std::cout << m << std::endl;
+}
+
+int main(void)
+{
+
+    testEigen();
 
     GLFWwindow* window;
 
@@ -34,11 +47,13 @@ int main(void) {
     if (!glfwInit())
         exit(EXIT_FAILURE);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
     window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window) {
+    if (!window)
+    {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
@@ -48,12 +63,60 @@ int main(void) {
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
-    while (!glfwWindowShouldClose(window)) {
+    // Initialize OpenGL loader
+    glewInit();
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    while (!glfwWindowShouldClose(window))
+    {
         glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        {
+            ImGui::Begin("Hello, world!");            // Create a window called "Hello, world!" and append into it.
+            ImGui::Text("This is some useful text."); // Display some text (you can use a format strings too)
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
     }
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwDestroyWindow(window);
-
     glfwTerminate();
+
     exit(EXIT_SUCCESS);
 }
